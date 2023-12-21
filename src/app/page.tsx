f4f52b53517d56ac661f3bc44b8376bc2793e0da
HyunYongJeong <jhy3579012@gmail.com>
2023-12-21 15:54:12 +0900

    추가 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,17 +163,6 @@ const Home: React.FC = () => {
 
   const [animateMatrix, setAnimateMatrix] = useState(false);
 
-  const generateRandomString = (length) => {
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  };
-
   return (
     <>
       <Header />
